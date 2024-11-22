c6f67cf1772077d32be6bbef13f3a856501c2116
SlytherinBrowser <146814140+SlytherinBrowser@users.noreply.github.com>
2024-11-22 12:58:53 +0100

    Update indexjs.js

--- indexjs.js
+++ indexjs.js
@@ -19,7 +19,7 @@ for (let i = 0; i < 50; i++) {
             snowflake.style.left = Math.random() * 100 + 'vw';
             snowflake.style.animationDelay = Math.random() * 5 + 's';
             document.body.appendChild(snowflake);
-        };
+        }
 
  function menuking() {
         window.location.href = "menu.html";
