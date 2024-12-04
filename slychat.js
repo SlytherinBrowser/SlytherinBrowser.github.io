57e3b2a6786b6891bc3689202e8ab26fe2929612
SlytherinBrowser <146814140+SlytherinBrowser@users.noreply.github.com>
2024-12-04 13:34:17 +0100

    Update slychat.js

--- slychat.js
+++ slychat.js
@@ -1,73 +1,75 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
 import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
-import { getFirestore, collection, addDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
+import { getFirestore, collection, getDocs, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
-// Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyA2l2SEcXPrLGpSOiTyyC55l35OMln1VUM",
-    authDomain: "slychat-e0a59.firebaseapp.com",
-    projectId: "slychat-e0a59",
-    storageBucket: "slychat-e0a59.appspot.com",
-    messagingSenderId: "462724887870",
-    appId: "1:462724887870:web:72d25e58b20af6e2d2286f",
-    measurementId: "G-52CMK3FJYW"
-};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-const db = getFirestore();
-const provider = new GoogleAuthProvider();
+    // Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyA2l2SEcXPrLGpSOiTyyC55l35OMln1VUM",
+        authDomain: "slychat-e0a59.firebaseapp.com",
+        projectId: "slychat-e0a59",
+        storageBucket: "slychat-e0a59.appspot.com",
+        messagingSenderId: "462724887870",
+        appId: "1:462724887870:web:72d25e58b20af6e2d2286f",
+        measurementId: "G-52CMK3FJYW"
+    };
 
-// Elementi
-const chatWindow = document.getElementById('chat-window');
-const chatInput = document.getElementById('chat-input');
-const sendButton = document.getElementById('send-button');
-const codeInput = document.getElementById('code-input');
-const joinButton = document.getElementById('join-button');
-const reserveButton = document.getElementById('reserve-button');
-const googleSignInButton = document.getElementById('google-sign-in-button');
-const googleSignOutButton = document.getElementById('google-sign-out-button');
-const userNamesContainer = document.getElementById('user-names');
-const popupContainer = document.getElementById('popup-container');
-const addEmailButton = document.getElementById('add-email');
-const emailContainer = document.getElementById('email-container');
-const saveButton = document.getElementById('save-button');
-const closeButton = document.getElementById('close-button');
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth();
+    const db = getFirestore();
+    const provider = new GoogleAuthProvider();
 
-let currentUser = null;  // Trenutni uporabnik
-let chatRoomCode = 'public'; // Privzeto javna seja
-const emailList = [];
+    // Elementi
+    const chatWindow = document.getElementById('chat-window');
+    const chatInput = document.getElementById('chat-input');
+    const sendButton = document.getElementById('send-button');
+    const codeInput = document.getElementById('code-input');
+    const joinButton = document.getElementById('join-button');
+    const reserveButton = document.getElementById('reserve-button');
+    const googleSignInButton = document.getElementById('google-sign-in-button');
+    const googleSignOutButton = document.getElementById('google-sign-out-button');
+    const userNamesContainer = document.getElementById('user-names');
+    const popupContainer = document.getElementById('popup-container');
+    const addEmailButton = document.getElementById('add-email');
+    const emailContainer = document.getElementById('email-container');
+    const saveButton = document.getElementById('save-button');
+    const closeButton = document.getElementById('close-button');
 
-// Funkcija za prikaz sporočila v oblačku
-function addMessageToChatWindow(message, sender, isSentByCurrentUser, status) {
-    const msgElement = document.createElement('div');
-    msgElement.classList.add('message-box');
+    let currentUser = null;  // Trenutni uporabnik
+    let chatRoomCode = 'public'; // Privzeto javna seja
+    const localMessages = [];  // Lokalno shranjevanje sporočil
+    let emailList = [];
 
-    const senderNameElement = document.createElement('div');
-    senderNameElement.classList.add('sender-name');
-    senderNameElement.textContent = sender;
+    // Funkcija za prikaz sporočila v oblačku
+    function addMessageToChatWindow(message, sender, isSentByCurrentUser, status) {
+        const msgElement = document.createElement('div');
+        msgElement.classList.add('message-box');
 
-    const msgTextElement = document.createElement('div');
-    msgTextElement.textContent = message.text;
+        const senderNameElement = document.createElement('div');
+        senderNameElement.classList.add('sender-name');
+        senderNameElement.textContent = sender;
 
-    const msgStatusElement = document.createElement('div');
-    msgStatusElement.classList.add('message-status');
-    msgStatusElement.textContent = status;
+        const msgTextElement = document.createElement('div');
+        msgTextElement.textContent = message.text;
 
-    if (isSentByCurrentUser) {
-        msgElement.classList.add('sent');
-    } else {
-        msgElement.classList.add('received');
+        const msgStatusElement = document.createElement('div');
+        msgStatusElement.classList.add('message-status');
+        msgStatusElement.textContent = status;
+
+        if (isSentByCurrentUser) {
+            msgElement.classList.add('sent');
+        } else {
+            msgElement.classList.add('received');
+        }
+
+        msgElement.appendChild(senderNameElement);
+        msgElement.appendChild(msgTextElement);
+        msgElement.appendChild(msgStatusElement);
+
+        chatWindow.appendChild(msgElement);
+        chatWindow.scrollTop = chatWindow.scrollHeight;  // Pomakni se navzdol do zadnjega sporočila
     }
-
-    msgElement.appendChild(senderNameElement);
-    msgElement.appendChild(msgTextElement);
-    msgElement.appendChild(msgStatusElement);
-
-    chatWindow.appendChild(msgElement);
-    chatWindow.scrollTop = chatWindow.scrollHeight;  // Pomakni se navzdol do zadnjega sporočila
-}
 
 // Google prijava
 googleSignInButton.onclick = () => {
@@ -75,7 +77,6 @@ googleSignInButton.onclick = () => {
         .then((result) => {
             const user = result.user;
             console.log("User signed in: ", user);
-            currentUser = user;
             updateUI(user);
         })
         .catch((error) => {
@@ -88,7 +89,6 @@ googleSignOutButton.onclick = () => {
     signOut(auth)
         .then(() => {
             console.log("User signed out.");
-            currentUser = null;
             updateUI(null);
         })
         .catch((error) => {
@@ -100,11 +100,9 @@ googleSignOutButton.onclick = () => {
 onAuthStateChanged(auth, (user) => {
     if (user) {
         console.log("User is signed in: ", user);
-        currentUser = user;
         updateUI(user);
     } else {
         console.log("No user is signed in.");
-        currentUser = null;
         updateUI(null);
     }
 });
@@ -115,83 +113,104 @@ function updateUI(user) {
         userNamesContainer.textContent = user.displayName || user.email;
         googleSignInButton.style.display = 'none';
         googleSignOutButton.style.display = 'block';
-        chatInput.disabled = false;
-        sendButton.disabled = false;
+        chatInput.disabled = 'false';
+        sendButton.disabled = 'false';
     } else {
         userNamesContainer.textContent = 'No user logged in';
         googleSignInButton.style.display = 'block';
         googleSignOutButton.style.display = 'none';
-        chatInput.disabled = true;
-        sendButton.disabled = true;
+        chatInput.disabled = 'true';
+        sendButton.disabled = 'true';
     }
 }
 
-// Chat sending functionality
-sendButton.onclick = async function () {
-    const message = chatInput.value;
-    if (message.trim() !== '') {
-        const chatMessage = {
-            text: message,
-            sender: currentUser ? currentUser.displayName || currentUser.email : 'Anonymous',
-            timestamp: new Date()
-        };
-
-        try {
-            // Shrani sporočilo v Firestore
-            await addDoc(collection(db, `chats/${chatRoomCode}`), chatMessage);
+    // Chat sending functionality
+    sendButton.onclick = function() {
+        const message = chatInput.value;
+        if (message.trim() !== '') {
+            const chatMessage = {
+                text: message,
+                sender: currentUser ? currentUser.displayName || currentUser.email : 'Anonymous'
+            };
+            db.ref(`chats/${chatRoomCode}`).push(chatMessage);
             addMessageToChatWindow(chatMessage, chatMessage.sender, true, 'Sent');
             chatInput.value = '';
-        } catch (error) {
-            console.error('Error sending message: ', error);
         }
-    }
-};
+    };
 
-// Joining a chat room
-joinButton.onclick = function () {
-    chatRoomCode = codeInput.value.trim();
-    if (chatRoomCode) {
-        const chatCollection = collection(db, `chats/${chatRoomCode}`);
-        onSnapshot(chatCollection, (snapshot) => {
-            snapshot.docChanges().forEach((change) => {
-                if (change.type === "added") {
-                    const message = change.doc.data();
-                    addMessageToChatWindow(message, message.sender, false, 'Received');
-                }
+    // Joining a chat room
+    joinButton.onclick = function() {
+        chatRoomCode = codeInput.value.trim();
+        if (chatRoomCode) {
+            db.ref(`chats/${chatRoomCode}`).on('child_added', (snapshot) => {
+                const message = snapshot.val();
+                addMessageToChatWindow(message, message.sender, false, 'Received');
             });
-        });
-    }
-};
+        }
+    };
 
-// Show the popup for reserving a session
-reserveButton.onclick = function () {
-    popupContainer.style.display = 'block';
-};
+    // Show the popup for reserving a session
+    reserveButton.onclick = function() {
+        popupContainer.style.display = 'block';
+    };
 
-// Close the popup
-closeButton.onclick = function () {
-    popupContainer.style.display = 'none';
-};
+    // Close the popup
+    closeButton.onclick = function() {
+        popupContainer.style.display = 'none';
+    };
 
-// Add email functionality
-addEmailButton.onclick = function () {
-    const emailInput = emailContainer.querySelector('input[type="email"]');
-    const email = emailInput.value.trim();
-    if (email) {
-        emailList.push(email);
-        const emailDiv = document.createElement('div');
-        emailDiv.textContent = email;
-        emailContainer.appendChild(emailDiv);
-        emailInput.value = ''; // Clear input field
-    }
-};
+    // Add email functionality
+    addEmailButton.onclick = function() {
+        const emailInput = emailContainer.querySelector('input[type="email"]');
+        const email = emailInput.value.trim();
+        if (email) {
+            emailList.push(email);
+            const emailDiv = document.createElement('div');
+            emailDiv.textContent = email;
+            emailContainer.appendChild(emailDiv);
+            emailInput.value = ''; // Clear input field
+        }
+    };
 
-// Save session functionality
-saveButton.onclick = function () {
-    const reservationCode = document.getElementById('reservation-code').value.trim();
-    if (reservationCode) {
-        console.log('Reservation Code:', reservationCode);
-        console.log('Email List:', emailList);
-        popupContainer.style.display = 'none'; // Close popup after saving
-    }
-};
+    // Save session functionality
+    saveButton.onclick = function() {
+        const reservationCode = document.getElementById('reservation-code').value.trim();
+        if (reservationCode) {
+            // Save the reservation code and emails to Firebase or any backend
+            console.log('Reservation Code:', reservationCode);
+            console.log('Email List:', emailList);
+            popupContainer.style.display = 'none'; // Close popup after saving
+        }
+    };
+
+
+
+
+        function isWinter() {
+            const now = new Date();
+            const month = now.getMonth();  // November is 10, December is 11, February is 1
+            const day = now.getDate();
+
+            // Preverimo, če je med 20. novembrom in 28. februarjem
+            if (month === 10 && day >= 20) return true; // november 20+
+            if (month === 11) return true; // december
+            if (month === 0) return true;  // januar
+            if (month === 1 && day <= 28) return true; // februar do 28
+            return false;
+        }
+
+        function createSnowflakes() {
+            if (isWinter()) {
+                for (let i = 0; i < 50; i++) {
+                    let snowflake = document.createElement('div');
+                    snowflake.className = 'snowflake';
+                    snowflake.textContent = '❄';
+                    snowflake.style.left = Math.random() * 100 + 'vw';
+                    snowflake.style.animationDelay = Math.random() * 5 + 's';
+                    document.body.appendChild(snowflake);
+                }
+            }
+        }
+
+        // Pokličemo funkcijo za ustvarjanje snežink
+        createSnowflakes();
