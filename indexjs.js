415aa7091cd86fc0b22f08f0665f141c594b6092
SlytherinBrowser <146814140+SlytherinBrowser@users.noreply.github.com>
2024-12-24 15:36:46 +0100

    Update indexjs.js

--- indexjs.js
+++ indexjs.js
@@ -711,6 +711,7 @@ let imageFile = null;
         "https://www.instagram.com/: "insta.png",
         "https://www.tiktok.com/": "tik.png",
     };
+ let url = '';
 
     // Prikaz modalnega okna ob kliku na gumb
     const modal = document.getElementById('myModal');
